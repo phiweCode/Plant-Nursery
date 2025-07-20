d2acb6d51f994e3df67fe419422cf2204d4abb3b
phiweCode <thabo.s.mngoma@gmail.com>
2025-07-20 20:59:03 +0200

    significant number of changes

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -9,7 +9,7 @@ function RootLayout() {
         <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
             <div className="md:flex md:items-center md:gap-12">
-              <Link className="block text-teal-600" to="/">
+              <Link className="block text-teal-600" to="/Plant-Nursery/">
                 <span className="sr-only">Home</span>
               <svg className="logo !fill-green-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M272 96c-78.6 0-145.1 51.5-167.7 122.5c33.6-17 71.5-26.5 111.7-26.5l88 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-16 0-72 0s0 0 0 0c-16.6 0-32.7 1.9-48.3 5.4c-25.9 5.9-49.9 16.4-71.4 30.7c0 0 0 0 0 0C38.3 298.8 0 364.9 0 440l0 16c0 13.3 10.7 24 24 24s24-10.7 24-24l0-16c0-48.7 20.7-92.5 53.8-123.2C121.6 392.3 190.3 448 272 448l1 0c132.1-.7 239-130.9 239-291.4c0-42.6-7.5-83.1-21.1-119.6c-2.6-6.9-12.7-6.6-16.2-.1C455.9 72.1 418.7 96 376 96L272 96z"/></svg>
               </Link>
@@ -19,7 +19,7 @@ function RootLayout() {
         <nav aria-label="Global">
           <ul className="flex items-center gap-6 text-sm">
             <li>
-              <Link to="/" className="text-gray-500 transition hover:text-gray-500/75" href="#"> Home </Link>
+              <Link to="/Plant-Nursery/" className="text-gray-500 transition hover:text-gray-500/75" href="#"> Home </Link>
             </li>
 
             <li>
